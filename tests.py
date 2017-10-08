1245ee30ffbded1c5c51022f9ad64c40f3c0a7d5
Rick Miyamoto <rkenmi@gmail.com>
2017-10-08 11:20:36 -0700

    add tests

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+from longest_increasing_subsequence import longest_increasing_subsequence
+
+
+class Lists(unittest.TestCase):
+    def test_longest_increasing_subsequence(self):
+        self.assertEquals(6, longest_increasing_subsequence([-9, -4, 8, -2, 1, 4, -1, 3, 4]))
+        self.assertEquals(2, longest_increasing_subsequence([-9, -4, 8, -2, 1, 4, -1, 3, 4]))
+
+
+if __name__ == '__main__':
+    unittest.main()
